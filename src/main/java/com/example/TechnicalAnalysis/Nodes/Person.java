4a60f73b614433e1dbe0998efc7d73d10aa17802
Kostas Thomson <kostasthomson12@gmail.com>
2023-07-29 14:16:47 +0300

    Sample data

--- src/main/java/com/example/TechnicalAnalysis/Nodes/Person.java
+++ src/main/java/com/example/TechnicalAnalysis/Nodes/Person.java
@@ -14,6 +14,11 @@ public class Person {
     private String firstName;
     private String lastName;
 
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/example/TechnicalAnalysis/TechnicalAnalysisApplication.java
+++ src/main/java/com/example/TechnicalAnalysis/TechnicalAnalysisApplication.java
@@ -1,7 +1,11 @@
 package com.example.TechnicalAnalysis;
 
+import com.example.TechnicalAnalysis.Nodes.Person;
+import com.example.TechnicalAnalysis.Repositories.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,4 +20,20 @@ public class TechnicalAnalysisApplication {
 	public static boolean isWindows() {
 		return System.getProperty("filename.separator").equals("\\");
 	}
+
+    @Bean
+    CommandLineRunner demo(PersonRepository personRepository) {
+        return args -> {
+            personRepository.deleteAll();
+
+            Person kostas = new Person("kostas", "thomson");
+            Person aristea = new Person("aristea", "kotoula");
+            Person kyriakos = new Person("kyriakos", "thomson");
+
+            personRepository.save(kostas);
+            personRepository.save(aristea);
+            personRepository.save(kyriakos);
+
+        };
+    }
 }
