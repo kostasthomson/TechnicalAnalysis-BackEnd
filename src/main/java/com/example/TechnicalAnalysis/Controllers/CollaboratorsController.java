b450a2a51fe640b8a7f8201c9e97a4a258688fe9
Kostas Thomson <kostasthomson12@gmail.com>
2023-09-18 02:01:19 +0300

    Cors access

--- src/main/java/com/example/TechnicalAnalysis/Controllers/CollaboratorsController.java
+++ src/main/java/com/example/TechnicalAnalysis/Controllers/CollaboratorsController.java
@@ -2,6 +2,7 @@ package com.example.TechnicalAnalysis.Controllers;
 
 import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Nodes.GitHubCollaborator;
 import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.CollaboratorRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //enable cors
 public class CollaboratorsController {
 
     private final CollaboratorRepository repository;
--- src/main/java/com/example/TechnicalAnalysis/Controllers/CommitsController.java
+++ src/main/java/com/example/TechnicalAnalysis/Controllers/CommitsController.java
@@ -2,6 +2,7 @@ package com.example.TechnicalAnalysis.Controllers;
 
 import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Nodes.GitHubCommit;
 import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.CommitRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") //enable cors
 public class CommitsController {
 
     private final CommitRepository repository;
