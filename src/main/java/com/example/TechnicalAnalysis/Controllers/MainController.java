2a987920353d85ef2930905f4f623d9867f990ed
Kostas Thomson <kostasthomson12@gmail.com>
2023-10-06 01:03:46 +0300

    initialization endpoint

--- src/main/java/com/example/TechnicalAnalysis/Controllers/MainController.java
+++ src/main/java/com/example/TechnicalAnalysis/Controllers/MainController.java
@@ -1,13 +1,40 @@
 package com.example.TechnicalAnalysis.Controllers;
 
+import com.example.TechnicalAnalysis.Services.ConnectorService.ConnectorController;
+import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.CollaboratorRepository;
+import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.CommitRepository;
+import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.GenericRepository;
+import com.example.TechnicalAnalysis.Services.GitHubService.Endpoints.EndpointsUtils.MapKeys;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MainController {
+    private final CollaboratorRepository collaboratorRepository;
+    private final CommitRepository commitRepository;
 
-    @GetMapping("/")
-    public String GetRootPath() {
-        return "Nothing to see here";
+    public MainController(CollaboratorRepository colRepo, CommitRepository comRepo) {
+        this.collaboratorRepository = colRepo;
+        this.commitRepository = comRepo;
+    }
+
+    @GetMapping("/init")
+    public String InitializeApplication(@RequestParam String link) {
+        this.collaboratorRepository.deleteAll();
+        this.commitRepository.deleteAll();
+
+        Map<MapKeys, GenericRepository<?, ?>> map = new HashMap<>();
+        map.put(MapKeys.COLLABORATORS, collaboratorRepository);
+        map.put(MapKeys.COMMITS, commitRepository);
+
+        ConnectorController controller = new ConnectorController(map);
+
+        controller.startAnalyzer();
+
+        return "Set up complete " + link;
     }
 }
--- src/main/java/com/example/TechnicalAnalysis/TechnicalAnalysisApplication.java
+++ src/main/java/com/example/TechnicalAnalysis/TechnicalAnalysisApplication.java
@@ -1,21 +1,11 @@
 package com.example.TechnicalAnalysis;
 
-import com.example.TechnicalAnalysis.Services.ConnectorService.ConnectorController;
-import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.CollaboratorRepository;
-import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.CommitRepository;
-import com.example.TechnicalAnalysis.Services.DatabaseService.DatabaseElements.Repositories.GenericRepository;
-import com.example.TechnicalAnalysis.Services.GitHubService.Endpoints.EndpointsUtils.MapKeys;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @EnableTransactionManagement
 @EnableNeo4jRepositories
@@ -30,27 +20,5 @@ public class TechnicalAnalysisApplication {
 
     public static boolean isWindows() {
         return System.getProperty("filename.separator").equals("\\");
-    }
-
-    public static void InitializeDB(CollaboratorRepository collaboratorRepository, CommitRepository commitRepository) {
-        collaboratorRepository.deleteAll();
-        commitRepository.deleteAll();
-
-        Map<MapKeys, GenericRepository<?, ?>> map = new HashMap<>();
-        map.put(MapKeys.COLLABORATORS, collaboratorRepository);
-        map.put(MapKeys.COMMITS, commitRepository);
-
-        ConnectorController controller = new ConnectorController(map);
-
-        controller.startAnalyzer();
-
-    }
-
-    @Bean
-    CommandLineRunner demo(CollaboratorRepository collaboratorRepository, CommitRepository commitRepository) {
-        return args -> {
-            InitializeDB(collaboratorRepository, commitRepository);
-            System.out.println("Set up completed");
-        };
     }
 }
