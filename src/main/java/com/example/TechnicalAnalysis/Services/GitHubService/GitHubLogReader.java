f117fc596d04267e6d499b3e02a05fa13894503b
Kostas Thomson <kostasthomson12@gmail.com>
2023-11-05 12:50:37 +0200

    Update GitHubLogReader.java
    Create analyzing commits log

--- src/main/java/com/example/TechnicalAnalysis/Services/GitHubService/GitHubLogReader.java
+++ src/main/java/com/example/TechnicalAnalysis/Services/GitHubService/GitHubLogReader.java
@@ -8,8 +8,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public abstract class GitHubLogReader {
     private static File LOG_FILE = null;
@@ -18,59 +16,74 @@ public abstract class GitHubLogReader {
         LOG_FILE = logFile;
     }
 
+    private static GitHubCommit createCommit(List<String> commitsList) {
+// TODO: ADD METHOD BODY -> CREATE A COMMIT ACCORDING TO LIST'S ELEMENTS
+        return null;
+    }
+
     public static void read() {
         try {
             BufferedReader br = new BufferedReader(new FileReader(LOG_FILE));
-            String line;
+
             List<GitHubCommit> commits = new ArrayList<>();
-            GitHubCommit currentCommit = null;
+            List<String> commitsStringList = new ArrayList<>();
 
-            Pattern commitPattern = Pattern.compile("^([0-9a-f]+),\\s(.*),\\s(.*),\\s(.+),\\s(.+)$");
-            Pattern filePattern = Pattern.compile("^([\\w/]+\\.java)$");
+            while (true) {
+                String line = br.readLine();
 
-            while ((line = br.readLine()) != null) {
-                Matcher commitMatcher = commitPattern.matcher(line);
-                if (commitMatcher.matches()) {
-                    if (currentCommit != null) {
-                        commits.add(currentCommit);
-                    }
+                if (line == null) break;
+                if (!line.isEmpty()) commitsStringList.add(line);
 
-                    String hash = commitMatcher.group(1);
-                    String authorName = commitMatcher.group(2);
-                    String authorEmail = commitMatcher.group(3);
-                    String date = commitMatcher.group(4);
-                    String message = commitMatcher.group(5);
-
-                    List<String> changedFiles = new ArrayList<>();
-                    while ((line = br.readLine()) != null && filePattern.matcher(line).matches()) {
-                        changedFiles.add(line);
-                    }
-
-                    currentCommit = new GitHubCommit(hash, authorName, authorEmail, date, message, changedFiles);
-                }
-            }
-
-            // Add the last commit if there is one
-            if (currentCommit != null) {
+                GitHubCommit currentCommit = createCommit(commitsStringList);
                 commits.add(currentCommit);
+                commitsStringList.clear();
             }
-
-            // Display the parsed commit information
-            for (GitHubCommit commit : commits) {
-                System.out.println("Hash: " + commit.getSha());
-                System.out.println("Author:<" + commit.getAuthorId() + ">");
-//                System.out.println("Date: " + commit.getDate());
-//                System.out.println("Message: " + commit.getMessage());
-//                System.out.println("Changed Files:");
-//                for (String file : commit.getChangedFiles()) {
-//                    System.out.println(file);
-//                }
-                System.out.println();
-            }
-
             br.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+
+//            Pattern commitPattern = Pattern.compile("^([0-9a-f]+),\\s(.*),\\s(.*),\\s(.+),\\s(.+)$");
+//            Pattern filePattern = Pattern.compile("^([\\w/]+\\.java)$");
+//
+//            while ((line = br.readLine()) != null) {
+//                Matcher commitMatcher = commitPattern.matcher(line);
+//                if (commitMatcher.matches()) {
+//                    if (currentCommit != null) {
+//                        commits.add(currentCommit);
+//                    }
+//
+//                    String hash = commitMatcher.group(1);
+//                    String authorName = commitMatcher.group(2);
+//                    String authorEmail = commitMatcher.group(3);
+//                    String date = commitMatcher.group(4);
+//                    String message = commitMatcher.group(5);
+//
+//                    List<String> changedFiles = new ArrayList<>();
+//                    while ((line = br.readLine()) != null && filePattern.matcher(line).matches()) {
+//                        changedFiles.add(line);
+//                    }
+//
+//                    currentCommit = new GitHubCommit(hash, authorName, authorEmail, date, message, changedFiles);
+//                }
+//            }
+//
+//            // Add the last commit if there is one
+//            if (currentCommit != null) {
+//                commits.add(currentCommit);
+//            }
+//
+//            // Display the parsed commit information
+//            for (GitHubCommit commit : commits) {
+//                System.out.println("Hash: " + commit.getSha());
+//                System.out.println("Author:<" + commit.getAuthorId() + ">");
+////                System.out.println("Date: " + commit.getDate());
+////                System.out.println("Message: " + commit.getMessage());
+////                System.out.println("Changed Files:");
+////                for (String file : commit.getChangedFiles()) {
+////                    System.out.println(file);
+////                }
+//                System.out.println();
+//            }
     }
 }
